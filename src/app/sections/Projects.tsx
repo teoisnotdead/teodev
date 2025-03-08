c110923529cc17ffe98e2b46408dcb3ced5e3ecf
TEO <alfredo.ssm@gmail.com>
2025-03-08 15:06:06 -0300

    Se tipa project cards

--- src/app/sections/Projects.tsx
+++ src/app/sections/Projects.tsx
@@ -1,22 +1,25 @@
 import { PROJECTS } from "../data";
 import { ProjectCard } from "../components";
+import { ProjectCardProps } from "../types";
+
+const projects: ProjectCardProps[] = PROJECTS;
 
 export const Projects = () => {
   return (
     <div id="projects" className="flex flex-col items-center mt-24 font-bold mx-auto w-full">
       <h2 className="uppercase text-5xl mb-8 leading-none">Proyectos</h2>
 
-      {PROJECTS.map((project) => (
-          <ProjectCard
-            key={project.title}
-            title={project.title}
-            description={project.description}
-            tags={project.tags}
-            image={project.image}
-            githubLink={project.githubLink}
-            demoLink={project.demoLink}
-          />
-        ))}
+      {projects.map(({ title, description, tags, image, githubLink, demoLink }) => (
+        <ProjectCard
+          key={title}
+          title={title}
+          description={description}
+          tags={tags}
+          image={image}
+          githubLink={githubLink}
+          demoLink={demoLink}
+        />
+      ))}
     </div>
   );
 };
